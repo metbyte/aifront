09069610dc79abb5ff879338b22d032e81b3e07e
cesarzijp <czijp@me.com>
2024-11-22 15:47:07 +0100

    niewe build

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: true,
+          compress: {
+            dead_code: true,
+            drop_debugger: true,
+            conditionals: true,
+            evaluate: true,
+            booleans: true,
+            loops: true,
+            unused: true,
+            hoist_funs: true,
+            keep_fargs: false,
+            hoist_vars: true,
+            if_return: true,
+            side_effects: true,
+            warnings: false,
+          },
+        },
+      }),
+    ],
+  },
+};
